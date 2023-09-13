1fa1aa7dab8d623fa5b14ef462dc291f9f09eaf2
mishra-rishabh <rishabh.ramg@gmail.com>
2023-09-14 01:10:36 +0530

    arrow function revisited

---
+++ 20_arrow_functions_revisited/arrow_function.js
@@ -0,0 +1,16 @@
+// 1st way
+/* const greet = () => {
+    console.log("Vande Mataram");
+} */
+
+// 2nd way
+/* const greet = () => console.log("Vande Mataram"); */
+
+// 3rd way, if parmeter is passed
+/* const greet = (name) => console.log("Vande Mataram " + name); */
+
+// 4th way, if parmeter is passed
+const greet = name => console.log("Vande Mataram " + name);
+
+// greet();
+greet("Rishabh");
